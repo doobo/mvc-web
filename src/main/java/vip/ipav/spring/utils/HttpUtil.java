e744447ab2e17f011601e495629e75a9578d04b1
doobo <doobo@foxmail.com>
2018-07-09 11:35:34 +0800

    OkHttpTools的常规同步写法

--- src/main/java/vip/ipav/spring/utils/HttpUtil.java
+++ src/main/java/vip/ipav/spring/utils/HttpUtil.java
@@ -1,24 +1,32 @@
 package vip.ipav.spring.utils;
 
+import okhttp3.Response;
 import vip.ipav.log.util.LoggerTools;
 import vip.ipav.okhttp.OkHttpClientTools;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HttpUtil {
 
     public static String sendHttpTest(){
+        Response response = null;
         try {
-            String html = OkHttpClientTools.getInstance().get()
+              response = OkHttpClientTools.getInstance().get()
                         .url("https://hao.360.cn/")
                         .addParam("src","lm")
                         .addParam("ls","n2a27c3f091")
-                        .execute().body().string();
+                        .execute();
+
+            String html = response.body().string();
             return html;
         } catch (IOException e) {
             LoggerTools.getCustomLogger().error("发送URL请求失败\n{}",e.getMessage());
+        } finally {
+            if(response != null){
+                response.close();
+            }
         }
-
         return null;
     }
 }
